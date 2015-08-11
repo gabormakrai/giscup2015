6bffcf78d250b21295b53b82fcd696014418c4aa
gabormakrai <makraigabor@gmail.com>
2015-08-11 16:03:15 +0100

    Initial c commit

---
+++ Giscup2015/src/Giscup2015.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Giscup2015.cpp
+// Author      : Gabor Makrai
+// Version     :
+// Copyright   : x
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
