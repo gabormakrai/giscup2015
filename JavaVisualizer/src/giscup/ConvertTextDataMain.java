60392c378b29def310873b480bbcd3f61adc6698
Gabor Makrai <makraigabor@gmail.com>
2015-08-19 16:36:11 +0100

    adding road conversion for JavaVisualization

--- JavaVisualizer/src/giscup/ConvertTextDataMain.java
+++ JavaVisualizer/src/giscup/ConvertTextDataMain.java
@@ -11,10 +11,11 @@ import java.io.OutputStreamWriter;
 public class ConvertTextDataMain {
 	
 	public static void main(String[] args) throws IOException {
-		new ConvertTextDataMain().run("d:\\giscup2015\\sfo_nodes.txt", "d:\\giscup2015\\sfo_nodes_qgis.csv");
+//		new ConvertTextDataMain().runNode("d:\\giscup2015\\sfo_nodes.txt", "d:\\giscup2015\\sfo_nodes_qgis.csv");
+		new ConvertTextDataMain().runRoad("d:\\giscup2015\\sfo_roads.txt", "d:\\giscup2015\\sfo_roads_qgis.csv");
 	}
 	
-	private void run(String inputNodeTextFile, String outputNodeCsvFile) throws IOException {
+	private void runNode(String inputNodeTextFile, String outputNodeCsvFile) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(inputNodeTextFile)));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputNodeCsvFile)));
@@ -22,7 +23,6 @@ public class ConvertTextDataMain {
 		String line = "";
 		
 		bw.write("id,x,y\n");
-		
 		
 		while ((line=br.readLine()) != null) {
 			String idString = line.substring(0, line.indexOf(":"));
@@ -35,4 +35,29 @@ public class ConvertTextDataMain {
 		br.close();
 		bw.close();
 	}
+	
+	private void runRoad(String inputRoadTextFile, String outputRoadCsvFile) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(inputRoadTextFile)));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputRoadCsvFile)));
+		
+		String line = "";
+		
+		bw.write("id;linestring\n");
+		
+		while ((line=br.readLine()) != null) {
+			String idString = line.substring(0, line.indexOf(":"));
+			String cString = line.substring(line.indexOf(">")+1);
+			cString = cString.substring(cString.indexOf(">")+1);
+			cString = cString.substring(0, cString.indexOf(" <"));
+			String[] coordinates = cString.split("\\ ");
+			String c0 = coordinates[0].replace(',', ' ');
+			String c1 = coordinates[coordinates.length - 1].replace(',', ' ');
+			bw.write(idString + ";LINESTRING(" + c0 + ", " + c1 + ")\n");
+		}
+		
+		br.close();
+		bw.close();
+	}
+	
 }
