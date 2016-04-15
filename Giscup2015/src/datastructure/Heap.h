8ea1b0297916762034da50605244fad2c374c915
gabormakrai <makraigabor@gmail.com>
2016-04-15 13:42:45 +0100

    adds a general heap class

---
+++ Giscup2015/src/datastructure/Heap.h
@@ -0,0 +1,25 @@
+/*
+ * ShortestPathAlgorithm.h
+ *
+ *  Created on: 18 Aug 2015
+ *      Author: makrai
+ */
+
+#ifndef _HEAP_H_
+#define _HEAP_H_
+
+template <class T>
+class Heap {
+public:
+	int size;
+	int* lookupTable;
+	int* nodeArray;
+
+	virtual ~Heap() { }
+
+	virtual void init(int sourceNode, T sourceValue) = 0;
+	virtual int extractMin() = 0;
+	virtual void decreaseKey(int node, T value) = 0;
+};
+
+#endif /* HEAP_H_ */
